b26d9aec690e78060e35c928bba0aa2406beafcd
Agatha cr <agatha.cr2004@aluno.ifsc.edu.br>
2023-08-08 09:03:52 -0300

    Aula 08/08/2023

---
+++ agatha/src/main/java/modelo/Bebida.java
@@ -0,0 +1,24 @@
+package modelo;
+
+public class Bebida {
+	
+	private int idbebida;
+	private String marca;
+	
+	public int getIdbebida() {
+		return idbebida;
+	}
+	public void setIdbebida(int idbebida) {
+		this.idbebida = idbebida;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	
+	
+
+}
